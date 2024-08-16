169af482cbf34bdf15c805e7c6d893e52b2cf794
Marina <marina.ionova@seznam.cz>
2024-08-16 15:24:33 +0200

    adopted for new pipeline

--- detector/detector.py
+++ detector/detector.py
@@ -4,105 +4,158 @@ import logging
 import cv2
 import os
 
+CROP_OFFSET = 3
+MIN_AREA = 200000
+MAX_AREA = 1000000
 
-class Detector:
-    def __init__(self, filename):
-        self.CROP_OFFSET = 3
-        self.MIN_AREA = 20000
-        self.SCALE_FACTOR = {'x': 1, 'y': 1}
-        self.filename = filename
-        self.image = self.get_image()
+# define the alpha and beta
+alpha = 0.92    # Contrast control
+beta = 10       # Brightness control
 
-    def get_image(self):
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        # Get the path to the 'scans' directory using the current file's location
-        scans_directory = os.path.join(script_dir, '..', 'data/scans')
 
-        # Specify the path to input file
-        tif_file_path = scans_directory + "/" + f"{self.filename}.tif"
+def show(image, name='Result'):
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.imshow(name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-        # Read the .tif file
-        image = cv2.imread(tif_file_path, cv2.IMREAD_UNCHANGED)
 
-        # Check if the image was successfully loaded
-        if image is not None:
-            scaled_image = cv2.resize(image, None, fx=self.SCALE_FACTOR['x'], fy=self.SCALE_FACTOR['y'])
-            return scaled_image
-        else:
-            logging.error(f"Unable to read the .tif file at {tif_file_path}")
-            raise FileExistsError
+def set_lim(dir_name):
+    if os.path.isfile(dir_name):
+        label = os.path.basename(dir_name)
+    else:
+        label = os.path.dirname(dir_name)
+    logging.debug(f"Label {label}")
+    if 'green' in label:
+        # GREEN
+        hsv_min = np.array((56, 66, 10), np.uint8)
+        hsv_max = np.array((100, 255, 188), np.uint8)
+        logging.info(f'Set hsv lim for green')
+    elif 'blue' in label:
+        # BLUE
+        hsv_min = np.array((28, 160, 73), np.uint8)
+        hsv_max = np.array((139, 255, 255), np.uint8)
+        logging.info(f'Set hsv lim for blue')
+    elif 'red' in label:
+        # RED
+        hsv_min = np.array((0, 166, 27), np.uint8)
+        hsv_max = np.array((180, 255, 255), np.uint8)
+        logging.info(f'Set hsv lim for red')
+    elif 'black' in label:
+        # BLACK
+        hsv_min = np.array((0, 0, 17), np.uint8)
+        hsv_max = np.array((179, 115, 76), np.uint8)
+        logging.info(f'Set hsv lim for black')
+    else:
+        # WHITE
+        hsv_min = np.array((0, 0, 221), np.uint8)  # 0, 0, 221
+        hsv_max = np.array((139, 11, 255), np.uint8)  # 139, 11, 255
+        logging.debug(f'Set hsv lim for white')
 
-    def save_image(self, image):
-        # Get the path to the 'scans' directory using the current file's location
-        output_directory = os.path.join(os.getcwd(), '..',  'data/outputs')
-        # Specify the path to output file
-        path_4_saving = output_directory + "/" + f"{self.filename}.png"
-        cv2.imwrite(path_4_saving, image)
+    return hsv_min, hsv_max
 
-    def image_preprocessing(self):  
-        # Provide morphological operations and find edges
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-        edged = cv2.Canny(blurred, 10, 100)
 
-        # Define a (3, 3) structuring element
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+def crop_image(masked_image, contour):
+    x, y, w, h = cv2.boundingRect(contour)
+    if y <= CROP_OFFSET and x <= CROP_OFFSET:
+        cropped_image = masked_image[y:(y + h + CROP_OFFSET), x:(x + w + CROP_OFFSET)]
+    elif y <= CROP_OFFSET:
+        cropped_image = masked_image[y:(y + h + CROP_OFFSET), x - CROP_OFFSET:(x + w + CROP_OFFSET)]
+    elif x <= CROP_OFFSET:
+        cropped_image = masked_image[y - CROP_OFFSET:(y + h + CROP_OFFSET), x:(x + w + CROP_OFFSET)]
+    else:
+        cropped_image = masked_image[y - CROP_OFFSET:(y + h + CROP_OFFSET),
+                        x - CROP_OFFSET:(x + w + CROP_OFFSET)]
+    return cropped_image
 
-        # Apply the dilation operation to the edged image
-        dilate = cv2.dilate(edged, kernel, iterations=1)
-        return dilate
 
-    def get_contours(self):
-        dilate = self.image_preprocessing()
-        # Contours detection
-        contours, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        image_copy = self.image.copy()
-        borders = []
-        # Contours drawing
-        for cnt in contours:
-            # Get the 4 points of the bounding rectangle
-            x, y, w, h = cv2.boundingRect(cnt)
-            if w * h > self.MIN_AREA:
-                # Draw a straight rectangle with the points
-                cv2.rectangle(image_copy, (x, y), (x + w, y + h), color=(255, 0, 0), thickness=2)
-                borders.append([x, y, w, h])
+def save_image(image, index, filename, output_dir):
+    if filename is None and os.path.isfile(output_dir):
+        filename = os.path.basename(output_dir).split('.')[0]
+        output_dir = os.path.dirname(output_dir)
+    else:
+        filename = filename.split('.')[0]
+        output_dir = os.path.join(output_dir, 'results')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-        return image_copy, contours, borders
+    # Specify the path to output file
+    cv2.imwrite(os.path.join(output_dir, f"{filename}_{index}.png"), image)
 
-    def crop(self, image, borders):
-        mats = []
-        for border in borders:
+
+def image_processing(hsv_min, hsv_max, input_dir, filename=None):
+    # Read the .tif file
+    if filename is None:
+        image = cv2.imread(input_dir, cv2.IMREAD_UNCHANGED)
+    else:
+        image = cv2.imread(os.path.join(input_dir, filename), cv2.IMREAD_UNCHANGED)
+
+    # Apply contrast and brightness control
+    adjusted = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+    # Apply color filter and invert it
+    hsv = cv2.cvtColor(adjusted, cv2.COLOR_BGR2HSV)
+    thresh = cv2.inRange(hsv, hsv_min, hsv_max)
+    inverted_thresh = cv2.bitwise_not(thresh)
+
+    # Find contours in the mask
+    mask_contours, _ = cv2.findContours(inverted_thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    mat_counter = 0
+    # Loop over the contours
+    for contour in mask_contours:
+        area = cv2.contourArea(contour)
+        if MIN_AREA < area:
+            inverted_thresh_copy = inverted_thresh.copy()
+            thresh_copy = thresh.copy()
+
+            # Detect the convex contour
+            hull = cv2.convexHull(contour)
+
+            # Convert hull points to the correct format for cv2.fillPoly
+            hull_points = np.array(hull, dtype=np.int32)
+
+            # Draw a filled polygon on the mask
+            cv2.fillPoly(inverted_thresh_copy, [hull_points], 255)
+            cv2.fillPoly(thresh_copy, [hull_points], 255)
+
+            # Apply the mask to the output to retain pixels outside the hull
+            final_mask = cv2.bitwise_and(thresh_copy, inverted_thresh_copy)
+
+            # Create a white image of the same size as the original image
+            white_image = np.ones_like(image) * 255
+
+            # Apply the mask: where mask is 0, use white_image; where mask is 1, use the original image
+            masked_image = np.where(final_mask[:, :, np.newaxis] == 0, white_image, image)
+
             # Cropping an image
-            x, y, w, h = border
-            if w * h > self.MIN_AREA:
-                cropped_image = image[y - self.CROP_OFFSET:(y + h + self.CROP_OFFSET),
-                                           x - self.CROP_OFFSET:(x + w + self.CROP_OFFSET)]
-                mats.append(cropped_image)
-        return mats
+            output_image = crop_image(masked_image, contour)
 
-    def get_masks(self, contours):
-        marks = []
-        for cnt in contours:
-            if cv2.contourArea(cnt) > 100000:
-                # Created a new mask and used bitwise_and to select for contours:
-                # hull = cv2.convexHull(cnt)
-                perimeter = cv2.arcLength(cnt, True)
-                approximatedShape = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
-                mask = np.zeros_like(self.image)
-                cv2.drawContours(mask, [approximatedShape], 0, (255, 255, 255), -1)
-                masked_image = cv2.bitwise_and(self.image, mask)
-                marks.append(masked_image)
-                self.show_image(mask)
-        return marks
+            save_image(output_image, mat_counter, filename, input_dir)
+            mat_counter += 1
 
-    def show_image(self, image, window_name='Result'):
-        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-        cv2.imshow(window_name, image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+
+def main(input_dir):
+    hsv_min, hsv_max = set_lim(input_dir)
+
+    if os.path.isdir(input_dir):
+        # Processing all images in the input directory
+        for filename in os.listdir(input_dir):
+            logging.info(f'Processing file {filename}')
+            if os.path.isfile(os.path.join(input_dir, filename)):
+                image_processing(hsv_min, hsv_max, input_dir, filename)
+
+    elif os.path.isfile(input_dir):
+        logging.info(f'Processing file {input_dir}')
+        # Processing one image
+        image_processing(hsv_min, hsv_max, input_dir)
+    else:
+        raise FileExistsError
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format=f"%(levelname)-8s: %(message)s")
+
     parser = argparse.ArgumentParser(
         prog='detector',
         description='This program applies image processing and contour '
@@ -110,35 +163,13 @@ if __name__ == '__main__':
                     'the processed image with drawn bounding rectangles '
                     'around detected shapes')
 
-    parser.add_argument('filename')  # positional argument
+    parser.add_argument('input_dir')  # positional argument
     parser.add_argument('-v', '--vis', help='show result', action='store_true')  # option that takes a value
-    parser.add_argument('-s', '--save', help='save result', action='store_true')  # option that takes a value
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format=f"%(levelname)-8s: %(message)s")
+    main(args.input_dir)
 
-    detector = Detector(args.filename)
-    result_image, contours, borders = detector.get_contours()
-    detector.show_image(result_image, 'Contours')
-    masks = detector.get_masks(contours)
-    mats = []
-    for mask, border in zip(masks, borders):
-        mat = np.array(detector.crop(mask, [border]))
-        for y in range(len(mat[0])):
-            for x in range(len(mat[0][y])):
-                if sum(mat[0][y][x]) == 0:
-                    mat[0][y][x] = [255, 255, 255]
 
-        mats.append(mat[0])
-        detector.show_image(mat[0], window_name='Contours')
 
-    logging.info(f'Summery: \nNumber of contours: {len(contours)}\n'
-                 f'Numer of borders: {len(borders)} \n'
-                 f'Number of  masks: {len(masks)}\n'
-                 f'Final number of mat: {len(mats)}')
 
-    if args.vis:
-        detector.show_image(result_image)
 
-    if args.save:
-        detector.save_image(result_image)
